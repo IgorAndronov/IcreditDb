d8f4a3631957b4fdd8c05018340567ce84b2fed1
Andreii Balanda <basf1@i.ua>
2016-01-31 23:03:28 +0200

    Insert citezenship

---
+++ flyway-3.2.1/sql/V171__INSERT_dic_sitezenship.sql
@@ -0,0 +1,8 @@
+--dic_citizenship
+
+INSERT INTO icredit.dic_citizenship(id, value, active)  VALUES(1040,980,1);
+
+insert into icredit.dic_values (id,name,comment,localisation) values(1040,'UKRAINE','UKRAINE','ENG');
+insert into icredit.dic_values (id,name,comment,localisation) values(1040,'УКРАИНА','УКРАИНА','RUS');
+insert into icredit.dic_values (id,name,comment,localisation) values(1040,'УКРАЇНА','УКРАЇНА','UKR');
+-----------------------------------------------------------
