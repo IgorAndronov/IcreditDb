029f8e5750ab41ca1919dd4cc015535d61f7a0bd
Andreii Balanda <basf1@i.ua>
2016-02-02 21:26:09 +0200

    Insert systempar and relation_group_n_to_page

---
+++ flyway-3.2.1/sql/V172__INSERT_system_parameters.sql
@@ -0,0 +1,9 @@
+﻿--syst m_param t rs
+delete from icredit.system_parameters where id in (3,4);
+
+INSERT INTO icredit.system_parameters ("id", "par_name", "value", "comment", "type_par", "active", "dic_name", "group_name")
+VALUES  (4,  'count_attempts_enter_password',  '3', 'count try entered incorrect password',  'integer', 1, NULL, 2);
+INSERT INTO icredit.system_parameters ("id", "par_name", "value", "comment", "type_par", "active", "dic_name", "group_name")
+VALUES  (3, 'time_locked_account_wrong_password', '5', 'time locked user login when hi write wrong password', 'integer', 1, NULL, 2);
+
+  --------------------------------------------
---
+++ flyway-3.2.1/sql/V173__INSERT_relation_group_n_to_page_n.sql
@@ -0,0 +1,17 @@
+﻿BEGIN;
+
+/* Data for the 'icredit.relation_group_n_to_page_n' table  (Records 1 - 9) */
+
+INSERT INTO icredit.relation_group_n_to_page_n ("contact_page_number", "contact_block_number", "sorting")
+VALUES
+  (1, 1, 1),
+  (1, 2, 2),
+  (1, 3, 3),
+  (1, 4, 4),
+  (2, 5, 1),
+  (3, 6, 1),
+  (4, 7, 1),
+  (5, 8, 1),
+  (6, 9, 1);
+
+COMMIT;
