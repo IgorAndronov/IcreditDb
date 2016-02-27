14d3f594c4d8539308a7a4ec4092c8e639605ae9
Andreii Balanda <basf1@i.ua>
2016-02-27 21:26:05 +0200

    update trch spech ALL

---
+++ flyway-3.2.1/sql/V205__upd_tech_spech_all.sql
@@ -0,0 +1,14 @@
+ALTER TABLE icredit.v_contacts_adress_tech_spec
+  RENAME COLUMN depends_chaild_column_name TO depends_child_column_name;
+
+  ALTER TABLE icredit.v_contacts_documents_tech_spec
+  RENAME COLUMN depends_chaild_column_name TO depends_child_column_name;
+
+  ALTER TABLE icredit.v_contacts_phones_tech_spec
+  RENAME COLUMN depends_chaild_column_name TO depends_child_column_name;
+
+  ALTER TABLE icredit.v_contacts_tech_spec
+  RENAME COLUMN depends_chaild_column_name TO depends_child_column_name;
+
+  ALTER TABLE icredit.v_contacts_www_tech_spec
+  RENAME COLUMN depends_chaild_column_name TO depends_child_column_name;
